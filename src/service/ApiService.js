e0a874fe767469c80cdc6246958d5c7697f32c1e
Mohd_Afsar <154755177+aitde36@users.noreply.github.com>
2025-08-15 02:02:44 +0530

    Update ApiService.js

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -3,7 +3,9 @@ import axios from "axios"
 
 export default class ApiService {
 
-    static BASE_URL = "http://localhost:4040"
+    static BASE_URL = "https://hotel-backend-production.up.railway.app"
+
+	//"http://localhost:4040"
 
     static getHeader() {
         const token = localStorage.getItem("token");
